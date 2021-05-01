3efea9ab8a76a30dd6008fd12ecee67b8d2b7a66
siva11ganesh <49832281+siva11ganesh@users.noreply.github.com>
2021-05-01 18:27:52 +0530

    Workspace recovered

--- DemoApiProject/src/com/api/booking/application/Application.java
+++ DemoApiProject/src/com/api/booking/application/Application.java
@@ -1,10 +1,7 @@
 package com.api.booking.application;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,8 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({ "com.api.booking" })
 @EnableCaching
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class })
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
